0510d14086fed49d40303628824e15530ae876d7
Rabe Aliza Mariz <rabealiza2003@gmail.com>
2023-04-20 17:02:26 +0800

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+SESSION_START();
+
+$acc_username = "alizarabe";
+$acc_password = "09152457";
+$acc_fullname = "Aliza Mariz Rabe";
+$acc_address = "Marinduque PH";
+
+
+$url_add = "http://" .$_SERVER['HTTP_HOST'] .$_SERVER['PHP_SELF'];
+
+
+
+if(isset($_REQUEST["login_button"])===true){
+	
+	if($_REQUEST['form_username'] != $acc_username){
+		header("Location: ".$url_add." ?notexist");
+	}
+	else if ($_REQUEST['form_username'] ==$acc_username && $_REQUEST['form_password'] != $acc_password){
+		header("Location:".$url_add." ?wrongpass");
+
+	}
+	else if ($_REQUEST['form_username'] ==$acc_username && $_REQUEST['form_password']){
+		header("Location:".$url_add." ?success");
+
+			$_SESSION['ses_username'] = $acc_username;
+			$_SESSION['ses_password'] = $acc_password;
+			$_SESSION['ses_fullname'] = $acc_fullname;
+			$_SESSION['ses_address'] = $acc_address;
+
+	}
+}
+
+?>
+
+
+
+
+
+
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,12 +67,37 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+				<form method="POST"  class="login-form">
+		      		
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+		      	<?php
+
+		      	if(isset($_REQUEST['notexist'])===true){
+		      		echo "<div class='alert alert-danger' role='alert'> Username does not exist....</div>";
+		      		} else if(isset($_REQUEST['wrongpass'])===true){
+		      			echo "<div class='alert alert-warning' role='alert'> Incorrect Password....</div>";
+
+		      		} else if(isset($_REQUEST['success']) ===true){
+		      			echo "<div class='alert alert-success' role='alert'> Redirecting....</div>";
+		      			header ("Refresh: 5; url=account.php");
+		      		}else if (isset($_REQUEST['logout']) === true){
+						echo "<div class='alert alert-info' role='alert'>Thank you...</div>";
+		      		} else if (isset($_REQUEST['logfirst']) === true){
+						echo "<div class='alert alert-info' role='alert'>Please Login First...</div>";
+		      		} else if (isset($_SESSION['ses_username'])===true){
+                  		echo "<div class='alert alert-info' role='alert'>You are still logged in. Please <a href='account.php'>click here</a> to proceed</div>";
+                  	}
+
+
+		      	
+
+		      	?>
+
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username"required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,7 +111,7 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5"name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
